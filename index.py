7411b71f28ec040123bf8332a5f6a988909a6ea4
zhizhuang.yang <zhizhuang.yang@lincomb.com.cn>
2017-09-28 21:14:04 +0800

    书籍购买系统

--- index.py
+++ index.py
@@ -9,6 +9,7 @@ books = [{"bookname":"《三体世界》","price":20.00,"num":5,"user":"admin"},
          {"bookname": "《黑暗森林》", "price": 30.00, "num": 8,"user":"admin"},
          {"bookname": "《死神永生》", "price": 50.00, "num": 20,"user":"admin"}]
 userAmount = [{"username":"admin","amount":100.00}]
+userBooks = []
 
 def index():
     print("********************************")
@@ -92,13 +93,19 @@ def main_index(username):
     print("********************************")
     print("*********XXX图书管理系统*********")
     print("1、选购书籍")
-    print("2、钱包管理")
+    print("2、我的书籍")
+    print("3、上传书籍")
+    print("4、查看余额")
     print("0、退出")
     print("********************************")
     choose = input("请选择：")
     if (choose == '1'):
         book_list(username)
-    elif (choose == '2'):
+    elif (choose=='2'):
+        book_manage(username)
+    elif (choose == '3'):
+        add_book(username)
+    elif (choose == '4'):
         amount_manage(username)
     elif (choose == '0'):
         index()
@@ -111,7 +118,7 @@ def book_list(username):
     print("*********书籍列表*********")
     print("\v\v编号\v\v书籍名称\v\v书籍价格\v\v剩余数量\v\v卖家")
     for i,book in enumerate(books):
-        print("\v\v%s"%str(i+1),"\v\v%s"%book["bookname"],"\v\v%s"%str(book["price"]),"\v\v%s"%str(book["num"]),"\v\v%s"%str(book["user"]))
+        print("\v\v%s"%str(i+1),"\v\v%s"%book["bookname"],"\v\v%s"%str(book["price"]),"\v\v%s"%str(book["num"]),"\v\v\v%s"%str(book["user"]))
     print("请输入书籍编号选购书籍，按0返回上一级")
     choose = input("请选择：")
     if (choose == '0'):
@@ -126,8 +133,8 @@ def book_list(username):
 def book_order(bookId,username):
     book = books[(int(bookId) - 1)]
     print("*********书籍订单*********")
-    print("书籍名称%s"%book["bookname"])
-    print("书籍单价%s"%str(book["price"]))
+    print("书籍名称:%s"%book["bookname"])
+    print("书籍单价:%s"%str(book["price"]))
     print("1、确认购买")
     print("0、返回上一级")
     choose = input("请选择：")
@@ -152,11 +159,11 @@ def checkBuyNum(book):
 def submit_order(username,bookId,num):
     book = books[(int(bookId) - 1)]
     print("*********提交订单*********")
-    print("书籍名称%s"%book["bookname"])
-    print("书籍单价%s"%str(book["price"]))
-    print("购买数量%s"%num)
+    print("书籍名称:%s"%book["bookname"])
+    print("书籍单价:%s"%str(book["price"]))
+    print("购买数量:%s"%num)
     priceAll = int(int(num) * int(book["price"]))
-    print("订单总价%s"%priceAll)
+    print("订单总价:%s"%priceAll)
     print("1、购买并支付")
     print("0、返回上一级")
     choose = input("请选择：")
@@ -191,11 +198,11 @@ def doOrder(username,bookId,num):
 
 
 def amount_manage(username):
-    print("*********钱包管理*********")
+    print("*********余额*********")
     print("0、退出")
     amountId = getAmountByUser(username)
     amount = userAmount[amountId]
-    print(amount)
+    print("余额:%s"%amount["amount"])
     choose = input("请选择：")
     if (choose == '0'):
         main_index(username)
@@ -208,6 +215,123 @@ def getAmountByUser(username):
         if(username==amount["username"]):
             return i
             break
+
+
+def book_manage(username):
+    print("************我的书籍*************")
+    print("\v\v编号\v\v书籍名称\v\v书籍价格\v\v剩余数量")
+    user_book_num = 0
+    clearUserBooks()
+    for i, book in enumerate(books):
+        if(book["user"]==username):
+            print("\v\v%s" % str(user_book_num + 1), "\v\v%s" % book["bookname"], "\v\v%s" % str(book["price"]),"\v\v%s" % str(book["num"]))
+            userBooks.append(book)
+            user_book_num+=1
+    print("请输入书籍编号进行操作，输入0返回上一级")
+    choose = input("请选择：")
+    if (choose == '0'):
+        main_index(username)
+    else:
+        if (re.match("[0-9]", choose) and int(choose) <= user_book_num):
+            book_detail(choose, username)
+        else:
+            print("输入编号有误，请重新选择")
+            book_manage(username)
+
+def clearUserBooks():
+    for i,book in enumerate(userBooks):
+        del userBooks[i]
+
+
+def book_detail(bookId,username):
+    book = userBooks[int(int(bookId)-1)]
+    print("************书籍详情******************")
+    print("书籍名称:%s" % book["bookname"])
+    print("书籍价格:%s" % str(book["price"]))
+    print("书籍数量:%s" % str(book["num"]))
+    print("1、编辑书籍")
+    print("2、删除书籍")
+    print("3、返回上一级")
+    print("0、退出")
+    choose = input("请选择：")
+    if (choose == '1'):
+        edit_book(username,bookId)
+    elif (choose == '2'):
+        del_book(bookId,username)
+    elif(choose=='3'):
+        book_manage(username)
+    elif(choose=='0'):
+        index()
+    else:
+        print("无效选择，请重新选择")
+        book_detail(bookId,username)
+
+def edit_book(username,bookId):
+    book = userBooks[int(int(bookId) - 1)]
+    print("************编辑书籍******************")
+    print("当前编辑书籍名称:%s"%book["bookname"])
+    bookname = input("请输入书籍名称:")
+    price = checkPrice()
+    num = checkBookNum()
+    new_book = {"bookname":bookname,"price":price,"num":num,"user":username}
+    books_id = getBookIdByBook(book)
+    books[books_id] = new_book
+    print("更改成功，返回到我的书籍")
+    book_manage(username)
+
+def checkPrice():
+    price = input("请输入书籍价格:")
+    if (re.match("[0-9]", price) ):
+        return price
+    else:
+        print("价格格式不正确，请重新输入")
+        checkPrice()
+
+def checkBookNum():
+    num = input("请输入书籍数量:")
+    if (re.match("[0-9]", num) ):
+        return num
+    else:
+        print("数量格式不正确，请重新输入")
+        checkBookNum()
+
+def add_book(username):
+    print("************新增书籍*************")
+    bookname = input("请输入书籍名称:")
+    price = checkPrice()
+    num = checkBookNum()
+    new_book = {"bookname": bookname, "price": price, "num": num, "user": username}
+    books.append(new_book)
+    print("新增成功，返回到我的书籍")
+    book_manage(username)
+
+def del_book(bookId,username):
+    book = userBooks[int(int(bookId) - 1)]
+    print("*************删除书籍**************")
+    print("当前编辑书籍名称:%s" % book["bookname"])
+    print("1、确认删除")
+    print("2、不删除，返回上一级")
+    print("3、退出")
+    choose = input("请选择:")
+    if(choose=='1'):
+        books_id = getBookIdByBook(book)
+        del books[books_id]
+        book_manage(username)
+    elif(choose=='2'):
+        book_manage(username)
+    elif(choose=='3'):
+        index()
+    else:
+        print("无效选择，请重新选择")
+        del_book(bookId, username)
+
+
+def getBookIdByBook(book):
+    for i,b in enumerate(books):
+        if(b==book):
+            return i
+            break
+
 
 
 
